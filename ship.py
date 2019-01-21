a8832d5fe3e08de80612ce2931344221940d5009
wangzhihaiSF <@1599315665@qq.com>
2019-01-21 20:07:29 +0800

    重构check_events

--- ship.py
+++ ship.py
@@ -1,8 +1,9 @@
 import pygame
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         # 初始化飞船及其位置
         self.screen = screen
+        self.ai_settings = ai_settings
         # 加载飞船的图像并获取其外接矩形
         # load 返回一个飞船的surface
         self.image = pygame.image.load("images/ship.bmp")
@@ -12,14 +13,22 @@ class Ship():
         # 将每个新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        # 在飞船的属性center中存储小数
+        self.center = float(self.rect.centerx)
 
         # 移动标志
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        # 根据移动标志调整飞船位置
-        if self.moving_right:
-            self.rect.centerx += 1
+        """根据移动标志调整飞船位置"""
+        # 更新飞船的 center 值，而不是rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        elif self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        # 根据 self.center 更新 rect 对象
+        self.rect.centerx = self.center
 
     def blit_me(self):
         # 指定位置绘制飞船
